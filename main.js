4ed77862249ab680a493ea29bee97230b0c322b2
Daniel <danielt014.ztm@gmail.com>
2024-04-12 19:55:27 +0300

    Work in progress, fetch pet data

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ async function start() {
 }
 
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  console.log(petsData)
+}
+
+petsArea()
